585d59bec756e592cd9e3565ec19dc4fa0c30e9b
KBFASHIONS <112752169+KBFASHIONS@users.noreply.github.com>
2023-01-03 22:38:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template
+from flask import *
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
-	return render_template("index.html")
+	return "index.html"
 	 
 if __name__ == '__main__':
 	app.run()
